d0cacf5219f3a8ba1d0e25e0fbad4f212eb2837f
matt4728 <50314810+matt4728@users.noreply.github.com>
2020-11-12 10:48:29 +0900

    C# 시작

---
+++ DataType/DataType/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isCSharp = true;
+            byte eightBit = 0xFF;
+
+        }
+    }
+}
