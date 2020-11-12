54b2586af8ec38c65af8c5546c7570ffad5810cd
matt4728 <50314810+matt4728@users.noreply.github.com>
2020-11-12 11:13:29 +0900

    변수 상수

---
+++ 변수 및 상수/변수 및 상수/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 변수_및_상수
+{
+    class Value
+    {
+        int a;
+        int b;
+        const int c = 1234;
+        readonly int d;
+        public Value(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+            d = 12345;
+        }
+
+        public void sum(int a, int b)
+        {
+            // error
+            //int localVal;
+            Console.WriteLine(a + b);
+            Console.ReadLine();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Value v = new Value(1, 2);
+            v.sum(123, 234);
+        }
+    }
+}
