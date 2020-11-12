758c852120a8de85037282a66fea330305b93d16
matt4728 <50314810+matt4728@users.noreply.github.com>
2020-11-12 10:54:41 +0900

    데이터 타입

--- DataType/DataType/Program.cs
+++ DataType/DataType/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DataType
 {
@@ -10,8 +6,18 @@ namespace DataType
     {
         static void Main(string[] args)
         {
-            bool isCSharp = true;
-            byte eightBit = 0xFF;
+            bool b = true;
+            byte by = byte.MaxValue;
+            short s = short.MaxValue;
+            int i = int.MaxValue;
+            long l = long.MaxValue;
+            float f = 3.14;
+            double d = 3.141592;
+            decimal de = decimal.MaxValue;
+            char ch = 'A';
+            string str = "asdf";
+            object obj = "아무거나";
+
 
         }
     }
